571c0af253be73b8371c2fdeeda4208ed501b4e0
Arthur Teixeira Serafim <arthurteixeiraserafim@gmail.com>
2023-06-09 22:54:52 -0300

    Calculadora feita com Constructor Function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,63 +4,39 @@ class Calculadora{
     constructor(){ //a classe Calculadora possui uma constructor
         //o this, aqui se refere a variavel que foi instanciada lá em baixo, a "calculadora", nesse caso
         this.display = document.querySelector('.display')
-
-        this.inicia = function(){
-            this.clickBotoes();
-            this.pressionaEnter()
+        this.inicia = () => {
+            clickBotoes()
+            pressionaEnter()
         };
-
-        this.pressionaEnter = function(){
-            this.display.addEventListener('keyup', e =>{
-                if(e.keyCode === 13){
-                    this.realizaConta();
-                }
+        const pressionaEnter = () => {
+            document.addEventListener('keypress', e =>{
+                if(e.keyCode !== 13) return; 
+                realizaConta();  
             })
         };
-
-        this.realizaConta = function(){
-            let conta = this.display.value;
+        const realizaConta = () => {
+            let conta = eval(this.display.value);
             try{
-                conta = eval(conta)
-                if(!conta){
-                    alert('Conta inválida!')
-                }
+                if(!conta) alert('Conta inválida!')
                 this.display.value = String(conta)
             } catch(e){
                 alert('Conta inválida')
+                return;
             }
         };
-
-        this.clearDisplay = function(){
-            this.display.value = ''
-        }
-
-        this.deleteOne = function(){
-            this.display.value = this.display.value.slice(0, -1)
-        };
-
-        this.clickBotoes = function(){
+        const clearDisplay = () => this.display.value = ''
+        const deleteOne = () => this.display.value = this.display.value.slice(0, -1)
+        const clickBotoes = () => {
             document.addEventListener('click', e =>{
                 const el = e.target;
-                if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText);
-                }
-                if(el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-                if(el.classList.contains('btn-del')){
-                    this.deleteOne();
-                }
-                if(el.classList.contains('btn-eq')){
-                    this.realizaConta();
-                }
+                if(el.classList.contains('btn-num')) btnParaDisplay(el.innerText);
+                if(el.classList.contains('btn-clear')) clearDisplay();
+                if(el.classList.contains('btn-del')) deleteOne();
+                if(el.classList.contains('btn-eq')) realizaConta();  
+                this.display.focus();
             })
         }
-
-        this.btnParaDisplay = function(valor){
-            this.display.value += valor
-        }
-
+        const btnParaDisplay = valor => this.display.value += valor
         /* Fora isso, tudo normal, como no ultimo projeto, mas aqui, por questão de sintaxe, os metodos
             são definidos de forma diferente, usando o 
             this.nomeDoMetedo = function(){
@@ -69,8 +45,6 @@ class Calculadora{
             *Lembrando que o this se refere sempre a constante que o objeto(ou classe) foi instanciado.
             Da pra ler como calculadora.metodo()
             */
-
-
     }
 }
 const calculadora = new Calculadora
